c1e326c2d4bcf03a453232d711af56a786003d3a
Mohamed <bouthourimed76@gmail.com>
2026-08-25 15:48:22 +0200

    install expo web

--- packages/design-system/src/primitives/PassengerSheet.tsx
+++ packages/design-system/src/primitives/PassengerSheet.tsx
@@ -197,6 +197,7 @@ const styles = StyleSheet.create({
   headerRow: {
     flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: spacing.xl,
   },
   headerSpacer: {
     width: spacing['3xl'],
